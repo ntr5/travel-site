4bbfc83209b6285d28d782b259c0328993e9a808
ntyr <ntorrs@gmail.com>
2018-04-03 23:10:58 -0600

    Complete large hero block and intro content block.

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+// organizing gulpfile.js 
+// when using the require function you don't need to include the .extension at the end of the line
+// importing all the specialized gulp files
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
